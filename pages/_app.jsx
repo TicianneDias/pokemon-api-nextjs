2432e4f6b1ce58a6c7f7ef2c9cce9d12a1dad1ed
Ticianne <ticidias@gmail.com>
2022-04-19 22:10:17 -0300

    page layout responsivity

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -5,6 +5,18 @@ import styled from 'styled-components';
 const Content = styled.div`
   width: 960px;
   margin: 0 auto;
+
+  @media (max-width: 987px) {
+    width: 700px;
+  }
+
+  @media (max-width: 720px) {
+    width: 450px;
+  }
+
+  @media (max-width: 470px) {
+    width: 350px;
+  }
 `;
 
 function MyApp({ Component, pageProps }) {
